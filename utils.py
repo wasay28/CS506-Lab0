2f5a56edd9a835887240b4aca53694909ab849c5
Wasay Rizwan <wasayr1@bu.edu>
2024-09-12 14:17:19 -0400

    Finished utils.py

--- utils.py
+++ utils.py
@@ -9,6 +9,7 @@ def dot_product(v1, v2):
     # Hint: use `np.dot`.
     '''
     ### YOUR CODE HERE
+    return np.dot(v1, v2)
     
 def cosine_similarity(v1, v2):
     '''
@@ -27,7 +28,12 @@ def cosine_similarity(v1, v2):
     # Hint: Use `dot_product` and `np.linalg.norm`.
     '''
     ### YOUR CODE HERE
+    dot_prod = dot_product(v1, v2)  
+    norm_v1 = np.linalg.norm(v1)    
+    norm_v2 = np.linalg.norm(v2)    
     
+    return dot_prod / (norm_v1 * norm_v2)
+
 def nearest_neighbor(target_vector, vectors):
     '''
     target_vector is a vector of shape d.
@@ -39,3 +45,15 @@ def nearest_neighbor(target_vector, vectors):
     # Hint: For this lab, you can just use a for loop to iterate through vectors.
     '''
     ### YOUR CODE HERE
+    best_similarity = -1  
+    best_index = -1
+
+    for i, vector in enumerate(vectors):
+        similarity = cosine_similarity(target_vector, vector)
+        if similarity > best_similarity:  
+            best_similarity = similarity
+            best_index = i
+    
+    return best_index
+
+
